a55758f50cf106ac6e05ded0c365b07db8ce7bee
Andy Parinas <andy.parinas@gmail.com>
2019-07-05 11:44:39 +1000

    Dev Tools initial commit

---
+++ lib/parinas_view_tool.rb
@@ -0,0 +1,7 @@
+require "parinas_view_tool/version"
+require "parinas_view_tool/renderer"
+
+module ParinasViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/parinas_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ParinasViewTool
+  VERSION = "0.1.0"
+end
